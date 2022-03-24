62312eae371fc4f96645b4efa0241e6e04cf3842
Aleksei.Suhhovski <suhhovski@gmail.com>
2022-03-24 22:06:10 +0200

    add new projects

---
+++ DataFromFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Lenovo\Projects\foods.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            foreach(string element in dataFromFile)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ FunctionsReturnAndArraysFromFile/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FunctionsReturnAndArraysFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DispalyRandomFood();
+            DisplayRandomDrink();
+            DisplayRandomMovie();
+        }
+
+
+        private static void DispalyRandomFood()
+        {
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, foods.Length);
+
+            string randomFood = foods[randomIndex];
+            Console.WriteLine($"Computer picked: {randomFood}");
+        }
+
+        private static void DisplayRandomDrink()
+        {
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite"};
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, drinks.Length);
+
+            string randomDrink = drinks[randomIndex];
+            Console.WriteLine($"Computer picked: {randomDrink}");
+        }
+
+        private static void DisplayRandomMovie()
+        {
+            string[] movie = { "The Hurt Locker", "The Godfather", "Pulp Fiction", "Saving Private Ryan", "Avatar" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, movie.Length);
+
+            string randomMovie = movie[randomIndex];
+            Console.WriteLine($"Computer picked: {randomMovie}");
+        }
+    }
+}
